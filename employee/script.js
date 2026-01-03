b7f017fe8924cc00e719d20f2314213bb5d20a4c
Kavya Ajagia <kavya.ajagia@gmail.com>
2026-01-03 10:35:34 +0530

    add employee folder

---
+++ employee/script.js
@@ -0,0 +1,17 @@
+function openProfile() {
+    alert("Opening Profile Page");
+}
+
+function openAttendance() {
+    alert("Opening Attendance Page");
+}
+
+function openLeave() {
+    alert("Opening Leave Request Page");
+}
+
+function logout() {
+    alert("Logged out successfully");
+   window.location.href = "../index.html";
+ // future login page
+}

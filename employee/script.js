396998103bafb36ae3b198b7a681fee928d70f4a
Kavya Ajagia <kavya.ajagia@gmail.com>
2026-01-03 14:52:32 +0530

    Update script.js

--- employee/script.js
+++ employee/script.js
@@ -1,17 +1,19 @@
 function openProfile() {
     alert("Opening Profile Page");
+     window.location.href = "profile.html";
 }
 
 function openAttendance() {
     alert("Opening Attendance Page");
+     window.location.href = "attendance.html";
 }
 
 function openLeave() {
     alert("Opening Leave Request Page");
+     window.location.href = "leave.html";
 }
 
 function logout() {
     alert("Logged out successfully");
    window.location.href = "../index.html";
- // future login page
 }
